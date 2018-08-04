1f12f7fb15dacc33877f85556083940cc0b3dd0c
CalinArdelean <calin.a@hotmail.com>
2018-08-04 11:56:46 -0400

    Update

--- Assignment 3/ext2.h
+++ Assignment 3/ext2.h
@@ -246,6 +246,20 @@ struct ext2_dir_entry_2 {
 
 #define DEBUG_LEVEL0
 
+#ifdef WIN32
+#ifdef DEBUG_EN
+#define LOG(level, format, ...) printf(format, __VA_ARGS__)
+#else
+#define LOG(level, format, ...)
+#endif
+
+#ifdef TRACE_EN
+#define TRACE(level, format, ...) printf(format, __VA_ARGS__)
+#else
+#define TRACE(level, format, ...)
+#endif
+
+#else  //Linux
 #ifdef DEBUG_EN
 #define LOG(level, format, ...) printf(format, ##__VA_ARGS__)
 #else
@@ -256,6 +270,7 @@ struct ext2_dir_entry_2 {
 #define TRACE(level, format, ...) printf(format, ##__VA_ARGS__)
 #else
 #define TRACE(level, format, ...)
+#endif
 #endif
 
 
